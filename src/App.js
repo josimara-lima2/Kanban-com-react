f6c53defe7e8b8998a2feb8799b37da43966854b
Josimara Silva <73074292+josimara-lima2@users.noreply.github.com>
2021-10-15 21:37:29 -0300

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,75 @@
+import React, { useState } from "react";
+import "./styles.css";
+import Navbar from "./Components/Navbar/Navbar";
+import TaskList from "./Components/TaskList/TaskList";
+
+let idAcc = 0;
+
+const generateId = () => {
+  idAcc = idAcc + 1;
+  return idAcc;
+};
+
+export default function App() {
+  const [tasks, setTasks] = useState([]);
+
+  const addTask = (title, state) => {
+    const newTask = {
+      id: generateId(),
+      title,
+      state
+    };
+    setTasks((existingTasks) => {
+      return [...existingTasks, newTask];
+    });
+  };
+
+  const updateTask = (id, title, state) => {
+    setTasks((existingTasks) => {
+      return existingTasks.map((task) => {
+        if (task.id === id) {
+          return { ...task, title, state };
+        } else {
+          return task;
+        }
+      });
+    });
+  };
+
+  const deleteTask = (id) => {
+    setTasks((existingTasks) => {
+      return existingTasks.filter((task) => task.id !== id);
+    });
+  };
+  return (
+    <div className="App">
+      <Navbar />
+      <div className="container">
+        <TaskList
+          title="Pendente"
+          onAddTask={addTask}
+          taskState="Pendente"
+          tasks={tasks.filter((t) => t.state === "Pendente")}
+          onTaskUptade={updateTask}
+          onDeleteTask={deleteTask}
+        />
+        <TaskList
+          title="Fazendo"
+          onAddTask={addTask}
+          taskState="Fazendo"
+          tasks={tasks.filter((t) => t.state === "Fazendo")}
+          onTaskUptade={updateTask}
+          onDeleteTask={deleteTask}
+        />
+        <TaskList
+          title="Completo"
+          onAddTask={addTask}
+          taskState="Completo"
+          tasks={tasks.filter((t) => t.state === "Completo")}
+          onTaskUptade={updateTask}
+          onDeleteTask={deleteTask}
+        />
+      </div>
+    </div>
+  );
+}
---
+++ src/Components/TaskItem/taskItem.js
@@ -0,0 +1,65 @@
+import React, { useState } from "react";
+import Proptypes from "prop-types";
+import "./taskItem.css";
+
+export default function TaskItem({
+  id,
+  title,
+  taskState,
+  onTaskUptade,
+  onDeleteTask
+}) {
+  const [isEditing, setIsEditing] = useState(false);
+  const [editableTitle, setEditableTitle] = useState(title);
+
+  const OnTitleChange = (event) => {
+    const newTitle = event.target.value;
+    setEditableTitle((existingEditableTitle) => {
+      return newTitle;
+    });
+    onTaskUptade(id, newTitle, taskState);
+  };
+
+  const onkeyPress = (event) => {
+    if (event.key === "Enter") {
+      setIsEditing(false);
+      if (editableTitle.length === 0) {
+        onDeleteTask(id);
+      }
+    }
+  };
+
+  const onTaskState = (event) => {
+    onTaskUptade(id, title, event.target.value);
+  };
+
+  if (isEditing) {
+    return (
+      <div className="task-item">
+        <input
+          type="text"
+          value={editableTitle}
+          onChange={OnTitleChange}
+          onKeyPress={onkeyPress}
+        />
+      </div>
+    );
+  } else {
+    return (
+      <div className="task-item">
+        <div onClick={(e) => setIsEditing(true)}>{editableTitle}</div>
+        <select onChange={onTaskState} value={taskState}>
+          <option value="Pendente">Pendente</option>
+          <option value="Fazendo">Fazendo</option>
+          <option value="Completo">Completo</option>
+        </select>
+      </div>
+    );
+  }
+}
+
+TaskItem.proptypes = {
+  id: Proptypes.number.isRequired,
+  title: Proptypes.string.isRequired,
+  taskState: Proptypes.string.isRequired
+};
